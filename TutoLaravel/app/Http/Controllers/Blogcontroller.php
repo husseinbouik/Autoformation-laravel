f4b5322a95ae3f7c71fdc8e14e8810ea3a1116fc
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2023-11-02 07:52:48 +0100

    blade

--- TutoLaravel/app/Http/Controllers/Blogcontroller.php
+++ TutoLaravel/app/Http/Controllers/Blogcontroller.php
@@ -4,22 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class Blogcontroller extends Controller
 {
-    public function index(): Paginator {
-        return \App\Models\Post::paginate(5);
+    public function index(): View {
+        return view("blog.index",[
+            'posts' => \App\Models\Post::paginate(2),
+
+        ]);
+
+
 
     }
-    public function show(string $slug ,string $id): RedirectResponse | Post
+    public function show(string $slug ,string $id): RedirectResponse | View
     {
     $post = \App\Models\Post::findorfail($id);
      /* redirect  to the right url   */ 
     if ($post->slug != $slug) {
         return to_route('blog.show', ['slug' => $post->slug, 'id' => $post->id]);
             }
-            return $post ;
+            return view("blog.show",[
+                'post' => $post,
+            ]) ;
     }
 }
